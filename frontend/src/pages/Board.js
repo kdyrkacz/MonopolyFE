ad51e582da9e59068f020bca68c5fb4c371d496f
alexrojas431 <alexrojas431@gmail.com>
2020-10-27 20:11:14 -0500

    Initializing of frontend Adds basic file structure and fundamental pages like main and board.js

---
+++ frontend/src/pages/Board.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Board.css";
+
+function Board() {
+  return (
+    <div className="Board">
+      This is the board page, where players will see the game board.
+      Should see fundamental UI like game board and unique player info like held money
+      and properties.
+    </div>
+  );
+}
+
+export default Board;
